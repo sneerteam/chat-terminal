9e29da35d1d0195742ac7cf6637d8692f0d623fe
Rodrigo B. de Oliveira <rbo@acm.org>
2014-02-24 23:55:04 -0300

    :get-last-msgs => :whats-up

--- src/sneerteam/chat_terminal/main.clj
+++ src/sneerteam/chat_terminal/main.clj
@@ -31,7 +31,7 @@
                            (conj-message msg)
                            (recur (server-timeout))))
         timeout ([_] (do
-                       (>! server-out {:type :get-last-msgs})
+                       (>! server-out {:type :whats-up})
                        ; try again in 1 minute
                        (recur (timeout-minutes 1))))))))
 
